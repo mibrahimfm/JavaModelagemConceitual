7bedb43fe30e3453cf937a5414221ff93fc1df26
Matheus Ibrahim <matheusibrahimfm@gmail.com>
2019-02-19 13:30:26 -0300

    Produto e associação Produto_Categoria muitos para muitos

--- src/main/java/com/cursoMC/domain/Categoria.java
+++ src/main/java/com/cursoMC/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.cursoMC.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -15,6 +18,9 @@ public class Categoria implements Serializable {
 		@GeneratedValue(strategy=GenerationType.IDENTITY)
 		private Integer id;
 		private String nome;
+		
+		@ManyToMany(mappedBy="Categorias")
+		private List<Produto> produtos = new ArrayList<>();
 		
 		public Categoria() {}
 
@@ -40,6 +46,14 @@ public class Categoria implements Serializable {
 			this.nome = nome;
 		}
 
+		public List<Produto> getProdutos() {
+			return produtos;
+		}
+
+		public void setProdutos(List<Produto> produtos) {
+			this.produtos = produtos;
+		}
+		
 		@Override
 		public int hashCode() {
 			final int prime = 31;
@@ -70,6 +84,8 @@ public class Categoria implements Serializable {
 				return false;
 			return true;
 		}
+
+
 		
 		
 		
---
+++ src/main/java/com/cursoMC/domain/Produto.java
@@ -0,0 +1,118 @@
+package com.cursoMC.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+
+@Entity
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private Double preco;
+
+	@ManyToMany
+	@JoinTable(name="PRODUTO_CATEGORIA",
+			joinColumns=@JoinColumn(name="ProdutoID"),
+			inverseJoinColumns = @JoinColumn(name="CategoriaID")
+	)
+	private List<Categoria> Categorias = new ArrayList<>();
+	
+	public Produto() {}
+
+	public Produto(Integer id, String nome, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return Categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		Categorias = categorias;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Categorias == null) ? 0 : Categorias.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((preco == null) ? 0 : preco.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (Categorias == null) {
+			if (other.Categorias != null)
+				return false;
+		} else if (!Categorias.equals(other.Categorias))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (preco == null) {
+			if (other.preco != null)
+				return false;
+		} else if (!preco.equals(other.preco))
+			return false;
+		return true;
+	}
+	
+	
+}
